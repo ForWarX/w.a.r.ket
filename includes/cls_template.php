b76473b4e8bcb29019262562551ce43e9d434b1d
chyj4747 <chyj4747@163.com>
2015-09-10 16:16:18 -0400

    Top banner and Animation
    Top banner in the index page and animation in the bill payment page

--- includes/cls_template.php
+++ includes/cls_template.php
@@ -652,6 +652,10 @@ class cls_template
                     case 'strip_tags':
                         $p = 'strip_tags(' . $p . ')';
                         break;
+                        
+                    case 'print_r':
+				        $p='print_r('.$p.',true)';
+				        break;
 
                     default:
                         # code...
