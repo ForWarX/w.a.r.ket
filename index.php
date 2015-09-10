fc7610d119a10e31f0e4ff85698c9a8713032f8b
mrwaynewu <waynewjq@gmail.com>
2015-09-10 11:17:28 -0400

    222
    222

--- index.php
+++ index.php
@@ -260,7 +260,7 @@ if (!$smarty->is_cached('index.dwt', $cache_id))
     /* 页面中的动态内容 */
     assign_dynamic('index');
 }
-print("test!");
+print("test!!");
 $smarty->display('index.dwt', $cache_id);
 
 /*------------------------------------------------------ */
