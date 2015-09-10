4e58b97a41f8522678381daad90a6fbf26764492
mrwaynewu <waynewjq@gmail.com>
2015-09-10 10:10:23 -0400

    zailai
    zailai

--- index.php
+++ index.php
@@ -260,6 +260,7 @@ if (!$smarty->is_cached('index.dwt', $cache_id))
     /* 页面中的动态内容 */
     assign_dynamic('index');
 }
+print("haha");
 $smarty->display('index.dwt', $cache_id);
 
 /*------------------------------------------------------ */
